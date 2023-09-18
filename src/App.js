7db932f24b2ca803d5a0a9157fa144cabe53fead
Sebastian <sebastianloslocos@gmail.com>
2023-09-17 20:19:28 -0400

    Agregado el Header

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import './assets/css/base/base.css';
 import './assets/css/componentes/card.css'
+import Page404 from './pages/404error';
 import Home from './pages/Home'
 import Sobre from './pages/Sobre'
+import Header from './components/Header';
 
 import { Routes, Route} from 'react-router-dom';
 
 function App() {
   return (
     <>
+      <Header />
     <Routes>
       <Route path='/' element={<Home />}/>
-      <Route path='/Sobre' element={<Sobre />}/>
+      <Route path='/sobre' element={<Sobre />}/>
+      <Route path='*' element={<Page404 />}/>
     </Routes>
     </>
   );
